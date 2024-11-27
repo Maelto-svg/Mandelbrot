64af18436c7b1d8a069c6eb926b62d6760a587d6
Maelwen THOMAS <thomas.maelwen@gmail.com>
2024-11-27 19:11:17 +0100

    Program now generate image in the pbm format

--- mandel.c
+++ mandel.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
+#define WIDTH 5000
+#define HEIGHT 5000
+
 double map (int v, int imin, int imax, double omin, double omax);
 
 int main()
 {
     int maxIter = 100;
     int iter;
+
     int radius = 2;
-    int h = 50;
-    int w = 160;
+
+    int h = HEIGHT;
+    int w = WIDTH;
     int px;
     int py;
+
+    double xMin = -2.0;
+    double xMax = 0.48;
     double xn;
     double x0;
+
+    double yMin = -1.24;
+    double yMax = 1.24;
     double yn;
     double y0;
     double temp;
 
+    int img[HEIGHT][WIDTH];
+    FILE * fout;
+
     for(py = 0; py<h; py++){
         for(px = 0; px<w; px++){
-            x0 = map(px, 0, w, -2.0, 0.48);
-            y0 = map(h-py, 0, h, -1.24, 1.24);
+            x0 = map(px, 0, w, xMin, xMax);
+            y0 = map(h-py, 0, h, yMin, yMax);
             xn = x0;
             yn = y0;
             iter = 0;
@@ -31,15 +45,40 @@ int main()
                 xn = temp;
                 iter++;
             }
-            if (iter == maxIter){
-                printf("*");
+            img[py][px] = iter;
+        }
+    }
+    fout = fopen("mandel.pbm","w");
+    if (fout == NULL){
+        return 1;
+    }
+    fprintf(fout, "P1\n");
+    fprintf(fout, "#Image calculé avec %d itérations maximal.\n", maxIter);
+    fprintf(fout, "#Visualisé dans l'intervale [%f, %f] en x et [%f, %f] en y\n", xMin, xMax, yMin, yMax);
+    fprintf(fout,"%d %d\n",WIDTH,HEIGHT);
+
+    
+    iter = 1;
+    for(py = 0; py<h; py++){
+        for(px = 0; px<w; px++){
+            if (img[py][px] == maxIter){
+                fprintf(fout, "0");
             }
             else{
-                printf(" ");
+                fprintf(fout, "1");
+            }
+            if (iter == 70){
+                iter = 1;
+                fprintf(fout, "\n");
+            }
+            else {
+                fprintf(fout, " ");
+                iter++;
             }
         }
-        printf("\n");
     }
+
+    fclose(fout);
 
     return 0;
 }
