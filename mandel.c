089dfcdd6ca9741fe6ef1627c793f0775ac66954
Maelwen THOMAS <thomas.maelwen@gmail.com>
2024-11-27 19:35:45 +0100

    Added gitignore and dynamic memory allocation.

--- mandel.c
+++ mandel.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define WIDTH 5000
-#define HEIGHT 5000
+#define WIDTH 1000
+#define HEIGHT 1000
 
-double map (int v, int imin, int imax, double omin, double omax);
+double map(int v, int imin, int imax, double omin, double omax);
 
 int main()
 {
@@ -14,76 +15,82 @@ int main()
 
     int h = HEIGHT;
     int w = WIDTH;
-    int px;
-    int py;
+    int px, py;
 
     double xMin = -2.0;
     double xMax = 0.48;
-    double xn;
-    double x0;
+    double xn, x0;
 
     double yMin = -1.24;
     double yMax = 1.24;
-    double yn;
-    double y0;
+    double yn, y0;
     double temp;
 
-    int img[HEIGHT][WIDTH];
-    FILE * fout;
+    FILE *fout;
+    int **img = malloc(h * sizeof(int *));
+    int i;
+    if (img == NULL) {
+        fprintf(stderr, "Erreur : Allocation dynamique échouée pour img.\n");
+        return 1;
+    }
+    for (i = 0; i < h; i++) {
+        img[i] = malloc(w * sizeof(int));
+        if (img[i] == NULL) {
+            fprintf(stderr, "Erreur : Allocation dynamique échouée pour img[%d].\n", i);
+            return 1;
+        }
+    }
 
-    for(py = 0; py<h; py++){
-        for(px = 0; px<w; px++){
+    for (py = 0; py < h; py++) {
+        for (px = 0; px < w; px++) {
             x0 = map(px, 0, w, xMin, xMax);
-            y0 = map(h-py, 0, h, yMin, yMax);
+            y0 = map(h - py, 0, h, yMin, yMax);
             xn = x0;
             yn = y0;
             iter = 0;
 
-            while(xn*xn + yn*yn < radius && iter<maxIter){
-                temp = xn*xn - yn*yn + x0;
-                yn = 2*xn*yn +y0;
+            while (xn * xn + yn * yn < radius * radius && iter < maxIter) {
+                temp = xn * xn - yn * yn + x0;
+                yn = 2 * xn * yn + y0;
                 xn = temp;
                 iter++;
             }
             img[py][px] = iter;
         }
     }
-    fout = fopen("mandel.pbm","w");
-    if (fout == NULL){
+
+    fout = fopen("mandel.pbm", "w");
+    if (fout == NULL) {
+        fprintf(stderr, "Erreur : Impossible d'ouvrir le fichier.\n");
         return 1;
     }
     fprintf(fout, "P1\n");
-    fprintf(fout, "#Image calculé avec %d itérations maximal.\n", maxIter);
+    fprintf(fout, "#Image calculée avec %d itérations maximal.\n", maxIter);
     fprintf(fout, "#Visualisé dans l'intervale [%f, %f] en x et [%f, %f] en y\n", xMin, xMax, yMin, yMax);
-    fprintf(fout,"%d %d\n",WIDTH,HEIGHT);
+    fprintf(fout, "%d %d\n", w, h);
 
-    
     iter = 1;
-    for(py = 0; py<h; py++){
-        for(px = 0; px<w; px++){
-            if (img[py][px] == maxIter){
-                fprintf(fout, "0");
-            }
-            else{
-                fprintf(fout, "1");
-            }
-            if (iter == 70){
-                iter = 1;
+    for (py = 0; py < h; py++) {
+        for (px = 0; px < w; px++) {
+            fprintf(fout, "%d ", img[py][px] == maxIter ? 0 : 1);
+            if (iter++ == 70) {
                 fprintf(fout, "\n");
-            }
-            else {
-                fprintf(fout, " ");
-                iter++;
+                iter = 1;
             }
         }
     }
 
     fclose(fout);
 
+    for (i = 0; i < h; i++) {
+        free(img[i]);
+    }
+    free(img);
+
     return 0;
 }
 
-double map (int v, int imin, int imax, double omin, double omax)
+double map(int v, int imin, int imax, double omin, double omax)
 {
-    return (double)(v-imin)/(imax-imin)*(omax-omin) + omin;
+    return (double)(v - imin) / (imax - imin) * (omax - omin) + omin;
 }
